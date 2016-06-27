5539b940e12d7d3f774dda2fccb96e740b29b4e6
Vallenteer <kristiantokelvin@yahoo.co.id>
2016-06-27 14:44:46 +0700

    kamera ratio diatur jadi 16 : 9

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,8 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		// Set camera aspect ratio
+		Camera.main.aspect = 16f/9f;
 	}
 	
 	// Update is called once per frame
