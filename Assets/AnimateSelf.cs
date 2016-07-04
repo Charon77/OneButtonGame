2f63b0bee1a66c159aebf1567de519db25449b41
Evans Jahja <evans.jahja@yahoo.com>
2016-07-05 01:53:42 +0700

    Added Animate Self Script

---
+++ Assets/AnimateSelf.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateSelf : MonoBehaviour {
+
+	[SerializeField]
+	int frameSkip = 24;
+
+	int lastTriggerFrame = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var frameCount = Time.frameCount;
+		if (frameCount -  lastTriggerFrame > frameSkip)
+		{
+			GetComponent<AnimStepper>().Step();
+			lastTriggerFrame = frameCount;
+		}
+
+	}
+}
