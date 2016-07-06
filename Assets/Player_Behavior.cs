0d27601b208894bae3f8271cd5f5961cc83b7036
Vallenteer <kristiantokelvin@yahoo.co.id>
2016-07-06 12:36:12 +0700

    collider sudah bisa mati nyala di enemy
    player sudah bisa kena musuh

---
+++ Assets/Player_Behavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Behavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.gameObject.tag == "Enemy") {
+			Debug.Log ("GAME OVER ENTER");
+		}
+
+	}
+	void OnTriggerStay2D(Collider2D coll) {
+		if (coll.gameObject.tag == "Enemy") {
+			Debug.Log ("GAME OVER STAY");
+		}
+
+	}
+}
---
+++ Assets/des_collider_check.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class des_collider_check : MonoBehaviour {
+
+	public string nama_sprite;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log ( gameObject.GetComponent<SpriteRenderer> ().sprite.name);
+		if (gameObject.GetComponent<SpriteRenderer> ().sprite.name == nama_sprite) {
+			gameObject.GetComponent<Collider2D> ().enabled = false;
+
+		} else {
+			gameObject.GetComponent<Collider2D> ().enabled = true;
+
+			
+
+		}
+	
+	}
+}
