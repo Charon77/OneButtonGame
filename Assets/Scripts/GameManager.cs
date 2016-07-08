8347d917e3d486ef4f2f2bd29362baf53da24830
Evans Jahja <evans.jahja@yahoo.com>
2016-07-08 20:32:59 +0700

    Added score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,24 @@ public class GameManager : MonoBehaviour {
 
 	[SerializeField]
 	GameObject playerCharacter;
+
+	[SerializeField]
+	int MOVEMENT_SCORE;
+
+	[SerializeField]
+	int SELL_SCORE;
+
+	[SerializeField]
+	float SCORE_MULTIPLIER = 1.0f;
+
+	public int Score {
+		get{
+			return _score;	
+		}
+	}
+
+	private int _score;
+
 	// Use this for initialization
 	void Start () {
 		// Set camera aspect ratio
@@ -27,7 +45,12 @@ public class GameManager : MonoBehaviour {
 		playerCharacter.GetComponent<PlayerBehavior>().Step();
 
 		//playerCharacter.GetComponentInChildren<AnimStepper>().Step();
+		_score += (int)(SCORE_MULTIPLIER * MOVEMENT_SCORE);
 
+	}
 
+	public void ResetMultiplier()
+	{
+		SCORE_MULTIPLIER = 1.0f ;
 	}
 }
---
+++ Assets/TextBinder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using HelperStuffs;
+
+public class TextBinder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponent<Text>().text = Helper.getGameManager().Score.ToString();
+	}
+}
