c80ad11b3a923d14371a71a569589c2a8dd887d5
Vallenteer <kristiantokelvin@yahoo.co.id>
2016-07-09 19:26:34 +0700

    adding Costumer Behavior

---
+++ Assets/Scripts/CostumerBehavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CostumerBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.gameObject.tag == "Enemy_3"||coll.gameObject.tag == "Decor" ||coll.gameObject.tag == "Costumer" ) {
+			Destroy (gameObject);
+			//Debug.Log ("Decor Kena ENEMY");
+		}
+
+	}
+}
