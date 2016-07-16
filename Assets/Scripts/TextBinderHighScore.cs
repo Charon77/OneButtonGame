7ccd4ee0dc701e17ee764b1abd6a2ee245e9e909
Vallenteer <kristiantokelvin@yahoo.co.id>
2016-07-16 23:59:24 +0700

    Main Menu dan GameOver Apk Test4
    Main Menu animasi dan tampilan
    Gameover tampilan dlll

---
+++ Assets/Scripts/TextBinderHighScore.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using HelperStuffs;
+
+public class TextBinderHighScore : MonoBehaviour {
+
+	int lastScore = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		int score = PlayerPrefs.GetInt ("HIGH_SCORE"); //Helper.getGameManager().HighScore;
+
+		if (lastScore <= score)
+		{
+			gameObject.GetComponent<Text>().text = lastScore.ToString();
+
+			// Hopefully not buggy
+			// A E S T H E T I C
+			if ((float)score - lastScore> 200)
+			{
+				lastScore += 100;
+			}
+			if ((float)score - lastScore> 20)
+			{
+				lastScore += 10;
+			}
+			else
+			{
+				lastScore++;
+			}
+		}
+
+
+	}
+}
